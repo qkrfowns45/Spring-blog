5a103b1e6806d6349282497da6099801d4316d3d
박래준 <qkrfowns@naver.com>
2022-10-22 21:06:46 +0900

    initial commit

---
+++ src/main/java/com/newbietop/blog/NewbietopblogApplication.java
@@ -0,0 +1,13 @@
+package com.newbietop.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbietopblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbietopblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newbietop/blog/NewbietopblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newbietop.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbietopblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
