85d65de099148915a63cf20e59b80d103aee19b0
박래준 <qkrfowns@naver.com>
2022-11-28 21:30:11 +0900

    회원가입 하기 ajax통신 및 서비스 통신 완료

---
+++ src/main/java/com/newbietop/blog/controller/api/UserApiController.java
@@ -0,0 +1,28 @@
+package com.newbietop.blog.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newbietop.blog.dto.ResponseDto;
+import com.newbietop.blog.model.RoleType;
+import com.newbietop.blog.model.User;
+import com.newbietop.blog.service.UserService;
+
+@RestController
+public class UserApiController {
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/api/user")
+	public ResponseDto<Integer> save(@RequestBody User user) { // username,password,email외에 나머지는 알아서 들어간다.
+		System.out.println("UserApiController : save호출됨");
+		//실제로 DB에 insert를 하고 아래에서 return한다.
+		user.setRole(RoleType.USER);
+		int result = userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK,result);
+	}
+}
---
+++ src/main/java/com/newbietop/blog/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.newbietop.blog.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto<T> {
+	HttpStatus status;
+	T data;
+}
