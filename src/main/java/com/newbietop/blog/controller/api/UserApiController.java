2230dd6926863819ab7b67baf7f71dca84890c56
박래준 <qkrfowns@naver.com>
2022-11-29 21:54:19 +0900

    예외 처리 수정

--- src/main/java/com/newbietop/blog/controller/api/UserApiController.java
+++ src/main/java/com/newbietop/blog/controller/api/UserApiController.java
@@ -23,6 +23,6 @@ public class UserApiController {
 		//실제로 DB에 insert를 하고 아래에서 return한다.
 		user.setRole(RoleType.USER);
 		int result = userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK,result);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),result);
 	}
 }
--- src/main/java/com/newbietop/blog/dto/ResponseDto.java
+++ src/main/java/com/newbietop/blog/dto/ResponseDto.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseDto<T> {
-	HttpStatus status;
+	int status;
 	T data;
 }
