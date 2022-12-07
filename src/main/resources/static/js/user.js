2dcd9daa3e642477ef27d4551cbdbba40750821f
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2022-12-07 23:49:19 +0900

    블로그 프로젝트 회원수정-1

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,6 +4,10 @@ let index = {
 			this.save();
 		});
 		
+		$("#btn-update").on("click",()=>{ //funtion(){}, ()=>{} this를 바인딩하기 위해서!!
+			this.update();
+		});
+		
 	},
 	
 	save:function(){
@@ -32,6 +36,29 @@ let index = {
 			alert(JSON.stringify(error));
 		});
 	},
+	
+	update:function(){
+		//alert('user의 save함수 호출됨');
+		let data = {
+			id:$("#id").val(),
+			password:$("#password").val(),
+			email:$("#email").val()
+		}
+		
+	
+		$.ajax({
+			type:"PUT",
+			url:"/user",
+			data:JSON.stringify(data), //http body데이터
+			contentType:"application/json;charset=utf-8", //body데이터가 어떤 타입인지(MIME)
+			dataType:"json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든것이 String(생긴게 json이라면) => javascript오브젝트로 변경
+		}).done(function(resp){
+			alert("회원수정이 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	}
 	
 }
 
